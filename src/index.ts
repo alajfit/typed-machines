651915202860b78ae0d02731d41b700a5f417415
Alan Fitzpatrick <alan.fitzpatrick@jet.com>
2019-07-23 22:08:08 +0100

    Testing Typed state machines

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Machine } from 'xstate'
+
+enum TYPES {
+    ATOMIC = 'atomic',
+    COMPOUND = 'compound',
+    PARALLEL = 'parallel',
+    FINAL = 'final',
+    HISTORY = 'history'
+}
+
+interface MachineValue {
+    id: string,
+    type: TYPES
+}
+
+class xMachine {
+    private id: string
+    private initial: string
+    private context: object
+    private states: object
+    private type: TYPES
+    private transitions: []
+
+    constructor(
+        id: string,
+        type: TYPES = TYPES.COMPOUND
+    ) {
+        this.id = id
+        this.type = type
+    }
+
+
+}
+
